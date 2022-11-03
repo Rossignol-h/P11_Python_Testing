00842eca0cb177407622eb8bd6d5c4c6e5703f6e
Rossignol-h <ha.rossignol@laposte.net>
2022-11-03 15:48:07 +0100

    test(units/): add assert message in test_past_competition.py

--- tests/units/test_past_competition.py
+++ tests/units/test_past_competition.py
@@ -6,7 +6,7 @@ def test_purchase_futur_competition(client, mocker, clubs_fixture, competitions_
         GIVEN a connected secretary's club wants to book places in a futur competition.
         date of this competition = "2023-02-05 10:00:00".
         WHEN this secretary types: 5 places
-        THEN an error message displays, with status code:400 BAD REQUEST.
+        THEN places are pruchased with status code:200.
     """
     mocker.patch.object(server, 'clubs', clubs_fixture)
     mocker.patch.object(server, 'competitions', competitions_fixture)
@@ -29,3 +29,4 @@ def test_purchase_past_competition(client, mocker, clubs_fixture, competitions_f
     competition = competitions_fixture[1]['name']
     response = client.post('/purchasePlaces', data={'competition': competition, 'club': club, 'places': 5})
     assert response.status_code == 400
+    assert b'Sorry, this competition is over !' in response.data
